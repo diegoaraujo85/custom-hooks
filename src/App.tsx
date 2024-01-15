bfad5be27a6dcf732ba304c8382be83a641d5462
Diego Oliveira de Araujo <diego.dearaujo@ambevtech.com.br>
2024-01-15 15:27:36 -0300

    simple user list with useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,43 @@
 'use client'
 
-import { useReducer } from 'react'
+import { useEffect, useReducer, useState } from 'react'
 
 function App() {
-  // reducer = (state, action) => newState
-  const initialState = {
-    count: 0
+const [data, setData] = useState([])
+const [isLoading, setIsLoading] = useState(false)
+const [error, setError] = useState(null)
+  
+const handleFetch = async () => {
+  setIsLoading(true)
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+    const data = await response.json()
+    setData(data)
+  } catch (error) {
+    setError(error)
+  } finally {
+    setIsLoading(false)
   }
-  const reducer = (state, action) => {
-    switch (action.type) {
-      case 'increment':
-        return {
-          ...state,
-          count: state.count + 1
-        }        
-      case 'decrement':
-        return {
-          ...state,
-          count: state.count - 1
-        }        
-    
-      default:
-        return state
-    }
-  }
+}
 
-  const [state, dispatch] = useReducer(reducer, initialState);
+useEffect(() => {
+  handleFetch()
+},[])
 
   return (
     <div className='flex flex-col items-center justify-center h-screen bg-zinc-900 text-white gap-4'>
       <h2>useReducer</h2>
       
-      <div className='flex flex-row gap-4 items-center'>
-        <button onClick={() => dispatch({ type: 'decrement' })} className='bg-red-500 text-white p-2 mr-2 w-32 hover:bg-red-600'>Decrement</button>
-        <p>Count: {state.count}</p>
-        <button onClick={() => dispatch({ type: 'increment' })} className='bg-green-500 text-white p-2 mr-2 w-32 hover:bg-green-600'>Increment</button>
-      </div>
+      {error && !data.length && <p className='text-red-500'>{error.message}</p>}
+      {isLoading && !data.length && <p className='text-yellow-500'>Loading...</p>}
+      {data && 
+        <ul>
+          {data.map((user) => (
+            <li key={user.id}>{user.name}</li>
+          ))
+          }
+        </ul>
+      }
     </div>
   )
 }
