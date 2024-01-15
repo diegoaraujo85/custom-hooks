4e4b51cc2bf344d8b7ecfa7e8cc0921d33c32c22
Diego Oliveira de Araujo <diego.dearaujo@ambevtech.com.br>
2024-01-15 16:34:17 -0300

    criando meu hook personalizado com useReducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,83 @@
 'use client'
 
-import { useEffect, useReducer, useState } from 'react'
+import axios from 'axios'
+import { useCallback, useEffect, useReducer } from 'react'
 
-function App() {
-const [data, setData] = useState([])
-const [isLoading, setIsLoading] = useState(false)
-const [error, setError] = useState(null)
-  
-const handleFetch = async () => {
-  setIsLoading(true)
-  try {
-    const response = await fetch('https://jsonplaceholder.typicode.com/users')
-    const data = await response.json()
-    setData(data)
-  } catch (error) {
-    setError(error)
-  } finally {
-    setIsLoading(false)
+type StateProps<T> = {
+  data?: T;
+  error?: Error;
+  isLoading: boolean;
+}
+
+type ActionProps<T> = {type: 'LOADING'} | {type: 'FETCHING', payload: T} | {type: 'ERROR', payload: Error}
+
+type User = {
+  id: number
+  name: string
+}
+
+// hook
+function useFetch<T = unknown>(url: string) {
+  const initialState: StateProps<T> = {
+    data: undefined,
+    error: undefined,
+    isLoading: false,
+  }
+
+  const reducer = (state: StateProps<T>, action: ActionProps<T>) => {
+    switch (action.type) {
+      case 'LOADING':
+        return {...initialState, isLoading: true}
+
+      case 'FETCHING':
+        return {...initialState, data: action.payload, isLoading: false}
+
+      case 'ERROR':
+        return {...initialState, error: action.payload, isLoading: false}
+            
+      default:
+        return state
+    }
+  }
+
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  const handleFetch = useCallback(async () => {
+    dispatch({type: 'LOADING'})
+    try {
+      const response = await axios.get(url)      
+      dispatch({type: 'FETCHING', payload: response.data})
+    } catch (error) {
+      if(error instanceof Error) {
+        dispatch({type: 'ERROR', payload: error})
+      }
+      if(axios.isAxiosError(error) && error.response) {
+        dispatch({type: 'ERROR', payload: error as Error})
+      }
+    } 
+  }, [url])
+
+  useEffect(() => {
+    handleFetch()
+  },[handleFetch]) // ao usar uma função como dependencia do useEffect, deve colocar a função dentro de um useCallback
+
+  return {
+    data: state.data,
+    isLoading: state.isLoading,
+    error: state.error
   }
 }
 
-useEffect(() => {
-  handleFetch()
-},[])
+function App() {
 
+  const {data, error, isLoading} = useFetch<User[]>('https://jsonplaceholder.typicode.com/users')    
+  
   return (
     <div className='flex flex-col items-center justify-center h-screen bg-zinc-900 text-white gap-4'>
       <h2>useReducer</h2>
       
-      {error && !data.length && <p className='text-red-500'>{error.message}</p>}
-      {isLoading && !data.length && <p className='text-yellow-500'>Loading...</p>}
+      {error && !data && <p className='text-red-500'>{error.message}</p>}
+      {isLoading && !data && <p className='text-yellow-500'>Loading...</p>}
       {data && 
         <ul>
           {data.map((user) => (
